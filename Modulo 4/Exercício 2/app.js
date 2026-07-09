1ba187093dc0ca9afa769edf229b77c25e285ac6
JOÃO VICTOR HORST DE SOUZA <joao_horst-souza@estudante.sesisenai.org.br>
2026-07-09 20:26:00 -0300

    feat:Exercício 2

---
+++ Modulo 4/Exercício 2/app.js
@@ -0,0 +1,43 @@
+import pg from 'pg';
+import PromptSync from 'prompt-sync';
+
+const { Client } = pg;
+
+const client = new Client({
+    host:     'localhost',
+    port:     5432,
+    user:     'postgres',
+    password: 'root',
+    database: 'escola_db'
+});
+
+async function MediaAlunos() {
+
+    try {
+        await client.connect();
+
+        const mediaAlunos = await client.query('SELECT AVG(nota) FROM alunos');
+
+        console.log("Total de alunos:");
+
+        alunosTotal.rows.forEach(aluno => {
+            console.log(`${aluno.count}`);
+        })
+
+        const alunosNota = await client.query('SELECT AVG(nota) FROM alunos');
+
+        console.log("Média geral da turma:");
+
+        alunosNota.rows.forEach(aluno => {
+            console.log(`${aluno.avg}`)
+        })
+
+    } catch (erro) {
+        console.log('❌ Erro ao buscar alunos:', erro.message);
+
+    } finally {
+        await client.end();
+    }
+}
+
+listarAlunos();
